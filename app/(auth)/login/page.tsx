ffb8cd8edce52b1f72ffb90de84a2d7c4252e2db
Fizzybrew <alexander200814@mail.ru>
2026-08-25 14:38:22 +0300

    feat: add login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/auth/login-form";
+
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>Sign in</h1>
+      <p>Sign in to continue.</p>
+      <LoginForm />
+    </main>
+  );
+}
