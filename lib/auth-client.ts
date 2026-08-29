4178a2328112cf1ff0854e522c69b6f58ae93653
F1ZZY <alexander200814@mail.ru>
2026-08-29 13:34:26 +0300

    feat(auth): add passkey client plugin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,7 @@
-import { createAuthClient } from "better-auth/react";
-import { emailOTPClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react"
+import { passkeyClient } from "@better-auth/passkey/client"
+import { emailOTPClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-  plugins: [emailOTPClient()],
-});
+  plugins: [emailOTPClient(), passkeyClient()],
+})
